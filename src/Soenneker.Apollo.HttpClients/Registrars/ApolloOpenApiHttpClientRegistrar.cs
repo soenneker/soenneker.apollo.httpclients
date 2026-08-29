a2d6c8baf22ba05703b1dd27b80bc108a6b18710
soenneker <jake@soenneker.com>
2026-08-29 14:16:45 -0500

    Improve public API XML documentation

--- src/Soenneker.Apollo.HttpClients/Registrars/ApolloOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Apollo.HttpClients/Registrars/ApolloOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class ApolloOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="ApolloOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddApolloOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class ApolloOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="ApolloOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddApolloOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
